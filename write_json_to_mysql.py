ab3fc4fd5d6b2f89058a55c0c2e118a447ae2cfa
Kit Haywood <kithaywood53@gmail.com>
2021-06-06 09:23:35 +0100

    updated writer

--- write_json_to_mysql.py
+++ write_json_to_mysql.py
@@ -1,14 +1,35 @@
 import pandas as pd
 import json
+import os
+import mysql.connector
+import pymysql
+import sqlalchemy as db
+
 
 def loader(filename):
-    with open(filename) as f:
-        data = json.loads(f.read())
-    bid = pd.DataFrame(data['prices']['PX_BID'])
-    bid = bid.set_index('date')
-    return bid
+    res = pd.DataFrame()
+    file_list = []
+    for dirname,path,file in os.walk(os.getcwd()):
+        for f in file:
+            if f.split('.')[-1]=='json' and 'data' in f.split('.')[0] and 'meta' not in f.split('.')[0]:
+                file_list.append(f)
+    for file in file_list:
+        with open(file) as f:
+            data = json.loads(f.read())
+            f.close()
+        data = pd.DataFrame(data['prices']['PX_BID']).set_index('date')
+        res = pd.concat([res,data])
+    res = res.reset_index()
+    return res
+
+def writer(df):
+    engine = db.create_engine('mysql+mysqldb://root:limetree123@127.0.0.1:3306/YieldCurves')
+    df.to_sql('YieldCurves',con=engine,if_exists='replace')
+    engine.connect().close()
+    return None
+
 
 if __name__=="__main__":
     data = loader('ASdata.json')
-    print(data)
-    # print(data)
+    writer(data)
+    print('\n','Complete','\n')
